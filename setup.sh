909f9c7b30b02d3f033c68e8e6c05fb43992b481
shinonome11a <39215628+shinonome11a@users.noreply.github.com>
2020-12-27 15:41:16 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "alias cl='clear'" >> ${HOME}/.bashrc
+echo "alias cle='clear'" >> ${HOME}/.bashrc
+echo "alias clea='clear'" >> ${HOME}/.bashrc
+echo "alias ll='ls -alshF'" >> ${HOME}/.bashrc
+echo "alias reboot='sync && sync && reboot'" >> ${HOME}/.bashrc
+echo "alias poweroff='sync && sync && poweroff'" >> ${HOME}/.bashrc
+echo 'PS1="[\u@\h:\W]%"' >> ${HOME}/.bashrc
+source ${HOME}/.bashrc
+echo "alias cl='clear'" | sudo tee -a /root/.bashrc > /dev/null
+echo "alias cle='clear'" | sudo tee -a /root/.bashrc > /dev/null
+echo "alias clea='clear'" | sudo tee -a /root/.bashrc > /dev/null
+echo "alias ll='ls -alshF'" | sudo tee -a /root/.bashrc > /dev/null
+echo 'PS1="[\u@\h:\W]#"' | sudo tee -a /root/.bashrc > /dev/null
+echo "alias poweroff='sync && sync && poweroff'"  | sudo tee -a /root/.bashrc > /dev/null
+echo "alias reboot='sync && sync && reboot'"  | sudo tee -a /root/.bashrc > /dev/null
